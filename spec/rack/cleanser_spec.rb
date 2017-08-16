31696ee33881207653b6c4276966308609d0e2c2
Sebastian Skałacki <skalee@gmail.com>
2017-08-17 00:33:30 +0200

    Delete a placeholder test

--- spec/rack/cleanser_spec.rb
+++ spec/rack/cleanser_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rack::Cleanser do
   it "has a version number" do
     expect(Rack::Cleanser::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end

7de74cdf9f5cb33b682d8f75a83eba8f27aeb0ad
Sebastian Skałacki <skalee@gmail.com>
2017-09-03 00:15:24 +0200

    Define matchers with RSpec DSL

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,17 +1,20 @@
+RSpec::Matchers.define :be_coming_from_inner_app do
+  match do |response|
+    response.status == 200 && response.body == "Hello World"
+  end
+end
+
+RSpec::Matchers.define :be_an_error do |code|
+  match do |response|
+    # rubocop:disable Style/RescueModifier
+    parsed_json = (JSON.parse(response.body) rescue {})
+    # rubocop:enable Style/RescueModifier
+    response.status == code && parsed_json.has_key?("error_message")
+  end
+end
+
+# RSpec::Mocks matchers cannot be defined that easily.
 module CustomMatchers
-  def be_coming_from_inner_app
-    satisfy { |r| r.status == 200 && r.body == "Hello World" }
-  end
-
-  def be_an_error(code)
-    satisfy do |r|
-      # rubocop:disable Style/RescueModifier
-      parsed_json = (JSON.parse(r.body) rescue {})
-      # rubocop:enable Style/RescueModifier
-      r.status == code && parsed_json.has_key?("error_message")
-    end
-  end
-
   def have_been_called_with_headers(headers)
     have_received(:call).with(hash_including(headers))
   end
